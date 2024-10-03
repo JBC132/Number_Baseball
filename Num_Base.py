cfe00aa031d3793ab0d9f485aefff9a9c6dfb5a5
이진영 <jakerhee321@gmail.com>
2024-10-03 16:49:34 +0900

    Input Player Name

--- Num_Base.py
+++ Num_Base.py
@@ -1,2 +1,16 @@
 import random
 
+def start_game():
+    name1 = input("Player1 Name?: ")
+    name2 = input("Player2 Name?: ")
+
+    name1_num = input(name1 + "\'s number: ")
+    name1_num = input(name2 + "\'s number: ")
+
+    if random.randrange(0, 2) == 1:
+        print(name2, "goes first")
+    else:
+        print(name1, "goes first")
+
+
+start_game()
