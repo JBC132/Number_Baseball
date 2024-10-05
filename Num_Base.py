b45544cb2bd7662f033f89db91f340620f5b5509
이진영 <jakerhee321@gmail.com>
2024-10-05 17:45:51 +0900

    Guess Function

--- Num_Base.py
+++ Num_Base.py
@@ -1,6 +1,8 @@
 import random
 
 def start_game():
+    game_set = False
+
     name1 = input("Player1 Name?: ")
     name2 = input("Player2 Name?: ")
 
@@ -11,6 +13,34 @@ def start_game():
         print(name2, "goes first")
     else:
         print(name1, "goes first")
+
+
+
+
+
+
+
+    game_set = True
+
+def guess(num_list, origin_num):
+    # input guessed number
+    while True:
+        guessed_num = input("Guess a number: ")
+        if guessed_num in num_list:
+            print("Already guessed. Try another number")
+            continue
+        else:
+            strike_ball(guessed_num, origin_num)
+            break
+
+def strike_ball(guessed_num, origin_num):
+    # check if guessed number has strike or ball
+
+    # result = <strike_num> + "strike" + <ball_num> + "ball"
+    # return result
+    pass
+
+
 
 
 start_game()
