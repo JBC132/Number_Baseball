332d9ace138e160fd4f59698e11b3c7072cbec12
이진영 <jakerhee321@gmail.com>
2024-10-06 23:10:14 +0900

    Strike Ball Function

--- Num_Base.py
+++ Num_Base.py
@@ -7,7 +7,9 @@ def start_game():
     name2 = input("Player2 Name?: ")
 
     name1_num = input(name1 + "\'s number: ")
-    name1_num = input(name2 + "\'s number: ")
+    name2_num = input(name2 + "\'s number: ")
+
+    num_length = len(str(name1_num))
 
     if random.randrange(0, 2) == 1:
         print(name2, "goes first")
@@ -17,12 +19,9 @@ def start_game():
 
 
 
-
-
-
     game_set = True
 
-def guess(num_list, origin_num):
+def guess(num_list, origin_num, num_length):
     # input guessed number
     while True:
         guessed_num = input("Guess a number: ")
@@ -30,17 +29,26 @@ def guess(num_list, origin_num):
             print("Already guessed. Try another number")
             continue
         else:
-            strike_ball(guessed_num, origin_num)
+            strike_ball(guessed_num, origin_num, num_length)
             break
 
-def strike_ball(guessed_num, origin_num):
+
+def strike_ball(guessed_num, origin_num, num_length):
     # check if guessed number has strike or ball
+    strike_num = 0
+    ball_num = 0
+    guessed_num = list(map(int, str(guessed_num)))
+    origin_num = list(map(int, str(origin_num)))
 
-    # result = <strike_num> + "strike" + <ball_num> + "ball"
-    # return result
-    pass
+    for i in range(num_length):
+        if guessed_num[i] in origin_num:
+            ball_num += 1
 
+        if guessed_num[i] == origin_num[i]:
+            ball_num -= 1
+            strike_num += 1
 
-
+    result = str(strike_num) + "Strike " + str(ball_num) + "Ball"
+    print(result)
 
 start_game()
